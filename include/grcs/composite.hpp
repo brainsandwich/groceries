61683beff8cade15c40b3db9f0c945f46c22588c
brainsandwich <alexandre.beaudet@protonmail.ch>
2017-10-23 16:24:26 +0200

    Wayyyy better composites

--- include/grcs/composite.hpp
+++ include/grcs/composite.hpp
@@ -5,6 +5,7 @@
 
 #include <memory>
 #include <unordered_map>
+#include <map>
 #include <typeindex>
 
 namespace grcs {
@@ -12,66 +13,107 @@ namespace grcs {
 
 	struct Composite {
 		lut::UUID uuid = lut::UUID::Null;
-		std::unordered_map<std::type_index, lut::storage::handle_t> components;
-
 		static const Composite Null;
 
 		Composite() {}
 		Composite(const lut::UUID& uuid)
 			: uuid(uuid) {}
 
-		template <typename T, typename ... Args>
-		void attach(Args&& ... args) {
-			auto tid = std::type_index(typeid(T));
-			if (components.count(tid))
-				return;
+		bool isvalid() const;
+		void destroy();
 
-			components[tid] = Thesaurus::components_market->create<T>(std::forward<Args>(args) ...);
-			get<T>().parent = this;
-		}
+		template <typename T, typename ... Args> void attach(Args&& ... args);
+		template <typename T> void detach();
+		template <typename T> T& get();
+		template <typename T> const T& get() const;
+		template <typename T> bool contains() const;
+	};
 
-		template <typename T>
-		void detach() {
-			auto tid = std::type_index(typeid(T));
-			if (!components.count(tid))
-				return;
+	struct Component {
+		bool initialized = false;
+		bool enabled = true;
+		Composite parent = Composite::Null;
 
-			Thesaurus::components_market->release<T>(components[tid]);
-			components.erase(tid);
-		}
-
-		template <typename T>
-		T& get() {
-			auto tid = std::type_index(typeid(T));
-			return Thesaurus::components_market->get<T>(components[tid]);
-		}
-
-		template <typename T>
-		const T& get() const {
-			auto tid = std::type_index(typeid(T));
-			return Thesaurus::components_market->get<T>(components[tid]);
-		}
-
-		template <typename T>
-		bool contains() {
-			auto tid = std::type_index(typeid(T));
-			return components.count(tid) && Thesaurus::components_market->isvalid<T>(components[tid]);
-		}
+		virtual ~Component() {}
+		virtual void oninit() {}
+		virtual void onupdate() {}
+		virtual void ondestroy() {}
 	};
 
 	struct Thesaurus {
 		static std::unique_ptr<lut::storage::Market> components_market;
-		static std::unordered_map<lut::UUID, Composite> composites;
+		static std::unordered_map<std::type_index, uint8_t> priorities;
+		static std::map<lut::UUID, std::unordered_map<std::type_index, lut::storage::handle_t>> components;
+		static std::vector<lut::UUID> garbage;
 
 		static void init();
 		static void clear();
-		static Composite& createComposite();
+		static Composite createComposite();
 		static void destroyComposite(Composite& composite);
+		static void clearGarbage();
+		template <typename Func> static void foreach(const Func& func) {
+			for (const auto& cmp : components)
+				func(Composite(cmp.first));
+		}
+
+		static void updateComponents();
 	};
 
-	struct Component {
-		Composite* parent;
-		virtual ~Component() {}
-	};
+	template <typename T, typename ... Args>
+	void Composite::attach(Args&& ... args) {
+		auto tid = std::type_index(typeid(T));
+		if (Thesaurus::components[uuid].count(tid))
+			return;
+
+		Thesaurus::components[uuid][tid] = Thesaurus::components_market->create<T>(std::forward<Args>(args) ...);
+		get<T>().parent = *this;
+	}
+
+
+	template <typename T>
+	void Composite::detach() {
+		if (!Thesaurus::components.count(uuid))
+			throw std::exception(std::string("composite " + uuid.tostring() + " does not exist in thesaurus").c_str());
+
+		auto tid = std::type_index(typeid(T));
+		if (!components.count(tid))
+			return;
+
+		Thesaurus::components_market->release<T>(components[tid]);
+		components.erase(tid);
+	}
+
+	template <typename T>
+	T& Composite::get() {
+		if (!Thesaurus::components.count(uuid))
+			throw std::exception(std::string("composite " + uuid.tostring() + " does not exist in thesaurus").c_str());
+
+		auto tid = std::type_index(typeid(T));
+		if (!Thesaurus::components[uuid].count(tid))
+			throw std::exception(std::string("no component of type " + std::string(tid.name()) + " belongs to composite " + uuid.tostring()).c_str());
+
+		return Thesaurus::components_market->get<T>(Thesaurus::components[uuid][tid]);
+	}
+
+	template <typename T>
+	const T& Composite::get() const {
+		if (!Thesaurus::components.count(uuid))
+			throw std::exception(std::string("composite " + uuid.tostring() + " does not exist in thesaurus").c_str());
+
+		auto tid = std::type_index(typeid(T));
+		if (!Thesaurus::components[uuid].count(tid))
+			throw std::exception(std::string("no component of type " + std::string(tid.name()) + " belongs to composite " + uuid.tostring()).c_str());
+
+		return Thesaurus::components_market->get<T>(Thesaurus::components[uuid][tid]);
+	}
+
+	template <typename T>
+	bool Composite::contains() const {
+		if (!Thesaurus::components.count(uuid))
+			throw std::exception(std::string("composite " + uuid.tostring() + " does not exist in thesaurus").c_str());
+
+		auto tid = std::type_index(typeid(T));
+		return Thesaurus::components[uuid].count(tid) && Thesaurus::components_market->isvalid<T>(Thesaurus::components[uuid][tid]);
+	}
 
 }
--- src/composite.cpp
+++ src/composite.cpp
@@ -3,6 +3,17 @@
 
 namespace grcs {
 
+	bool Composite::isvalid() const {
+		return Thesaurus::components.count(uuid);
+	}
+
+	void Composite::destroy() {
+		if (!isvalid())
+			return;
+
+		Thesaurus::garbage.push_back(uuid);
+	}
+
 	void Thesaurus::init() {
 		components_market = std::make_unique<lut::storage::Market>();
 	}
@@ -11,21 +22,64 @@ namespace grcs {
 		components_market.release();
 	}
 
-	Composite& Thesaurus::createComposite() {
+	Composite Thesaurus::createComposite() {
 		lut::UUID uuid = lut::UUID::random();
-		composites[uuid] = Composite(uuid);
-		Pigeon::send("composite-created", uuid);
-		return composites[uuid];
+		Composite ret(uuid);
+		Thesaurus::components[uuid].clear();
+		//Pigeon::send("composite-created", uuid);
+		return ret;
 	}
 
 	const Composite Composite::Null;
 
 	void Thesaurus::destroyComposite(Composite& composite) {
-		for (auto& pair : composite.components)
+		for (auto& pair : components[composite.uuid])
 			components_market->release(pair.first, pair.second);
+		//Pigeon::send("composite-destroyed", composite.uuid);
+		components.erase(composite.uuid);
+		composite = Composite::Null;
+	}
+
+	void Thesaurus::clearGarbage() {
+		for (const auto& uuid : garbage) {
+			for (auto& pair : components[uuid])
+				components_market->release(pair.first, pair.second);
+			//Pigeon::send("composite-destroyed", uuid);
+			components.erase(uuid);
+		}
+		garbage.clear();
+	}
+
+	void Thesaurus::updateComponents() {
+		std::list<std::type_index> registered_types;
+		for (auto& pair : components_market->shelves)
+			registered_types.push_back(pair.first);
+
+		registered_types.sort([&](const std::type_index& left, const std::type_index& right) {
+			uint8_t prio_left = 0, prio_right = 0;
+			if (priorities.count(left)) prio_left = priorities[left];
+			if (priorities.count(right)) prio_right = priorities[right];
+			return prio_left < prio_right;
+		});
+
+		for (const auto& type : registered_types) {
+			//fprintf(stdout, "* updating components %s ...\n", type.name());
+			components_market->foreach(type, [](void* item) {
+				Component& component = *(Component*) item;
+				if (!component.initialized) {
+					component.oninit();
+					component.initialized = true;
+				}
+				component.onupdate();
+			});
+			//fprintf(stdout, "* updated %lu components\n", components_market->shelves[type]->size());
+		}
 	}
 
 	std::unique_ptr<lut::storage::Market> Thesaurus::components_market = nullptr;
-	std::unordered_map<lut::UUID, Composite> Thesaurus::composites;
+	//std::unordered_map<lut::UUID, Composite> Thesaurus::composites;
+	std::map<lut::UUID, std::unordered_map<std::type_index, lut::storage::handle_t>> Thesaurus::components;
+	std::vector<lut::UUID> Thesaurus::garbage;
+	std::unordered_map<std::type_index, uint8_t> Thesaurus::priorities;
 	
 }
